d8952d927ebd507d0e4fe6b880a9d4abf19ed780
HolaJesus <116959112+HolaJesus@users.noreply.github.com>
2023-09-06 19:40:41 -0500

    Add files via upload

---
+++ Formulario-main/Formulario.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form name="formulario" method="post" action="Prueba.php">
+
+            Nombre: <input type="text" name="nombre" value="">
+            Apellido: <input type="text" name="apellido" value="">
+            Documento: <input type="text" name="documento" value="">
+            Edad: <input type="text" name="edad" value="">
+            Profesion: <input type="text" name="profesion" value="">
+            sexo: <input type="text" name="sexo" value="">
+
+            <input type="submit" />
+
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
---
+++ Formulario-main/Prueba.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$documento = $_POST['documento'];
+$edad = $_POST['edad'];
+$profesion = $_POST['profesion'];
+$sexo = $_POST['sexo'];
+
+echo 'Nombre:', $nombre ,' Apellido:', $apellido , ' Documento:', $documento, ' Edad:',  $edad, 'Profesion', $profesion, 'Sexo', $sexo; 
+
+?>
